f6f74acf41f1103699bc53703fd8d08ca7a2e740
chhotu <chhotu456787@gmail.com>
2024-07-09 17:19:55 +0530

    Description of changes

--- register.php
+++ register.php
@@ -1,24 +1,54 @@
 <html>
+
 <head>
-<link src= "style.css"> </link>
-<style>
-</style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registration form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link src="style.css">
+    </link>
+    <style>
+        .formstyle{
+            height: 30rem;
+            width: 30rem;
+            padding: 2rem;
+        }
+
+        @media (max-width: 320px) { 
+            .formstyle{
+                height: 30rem;
+                width: 20rem;
+            }
+         }
+    </style>
+</head>
 <body>
-Register Page
+    <div class="container formstyle">
+        <h1 style="text-align:center">Registration Form</h1>
+        <br>
 
-<form action="function.php" name="registerform1" id="registerform1" method="POST">
+        <form class="row g-3" action="function.php" name="registerform1" id="registerform1" method="POST">
+            <div class="col-md-12">
+                <label for="input" class="form-label">Username</label>
+                <input type="text" class=" form-control username" name="username" id="username">
+            </div>
+            <div class="col-md-12">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control pass" name="pass" id="pass">
+            </div>     
+            <div class="col-12" style="text-align:center">
+                    <input type="hidden" name="registerform" id="registerform" value="submitted">
+                <button style="height:3rem; width:7rem; font-size:1.5rem;" type="submit" class="btn btn-primary">Sign in</button>
+            </div>
+        </form>
+    </div>
 
-<Input type="text" name="username" id="username">
-<br>
-<input type="password" name="pass" id="pass">
-<br>
-<input type="hidden" name="registerform" id="registerform" value="submitted">
-<br>
-<input type="submit">
-
-</form>
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
 </body>
-
-
-
+</html>
--- signin.php
+++ signin.php
@@ -1,8 +1,8 @@
 <html>
 <head>
 <link src= "style.css"> </link>
-<style>
-</style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script>
 
 function submit1()
@@ -13,11 +13,24 @@ function submit1()
 
 </script>
 
+<style>
+        .formstyle{
+            height: 30rem;
+            width: 30rem;
+            padding: 2rem;
+        }
 
+        @media (max-width: 320px) { 
+            .formstyle{
+                height: 30rem;
+                width: 20rem;
+            }
+         }
+    </style>
 
 </head>
 <body>
-Login Page
+<!-- Login Page
 
 <form action="function.php" name="signin" id="signin" method="POST">
 
@@ -29,9 +42,26 @@ Login Page
 <br>
 <input type="submit">
 
-</form>
+</form> -->
+
+<div class="container formstyle">
+        <h1 style="text-align:center">Login Form</h1>
+        <br>
+
+        <form class="row g-3" action="function.php" name="signin" id="signin" method="POST">
+            <div class="col-md-12">
+                <label for="input" class="form-label">Username</label>
+                <input type="text" class=" form-control username" name="username" id="username">
+            </div>
+            <div class="col-md-12">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control pass" name="pass" id="pass">
+            </div>     
+            <div class="col-12" style="text-align:center">
+                    <input type="hidden" name="signinform" id="signinform" value="submitted">
+                <button style="height:3rem; width:7rem; font-size:1.5rem;" type="submit" class="btn btn-primary">Login </button>
+            </div>
+        </form>
+    </div>
 
 </body>
-
-
-
